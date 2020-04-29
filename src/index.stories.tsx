71b34760770d605ea691bb3a3055052e7254c813
Amit Rajput <amitrajput123.ar@gmail.com>
2020-04-30 01:01:13 +0530

    fixing index.stories.tsx

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
 
 export default { title: 'Button' };
 
-export const withText = () => <Button>Hello Button</Button>;
+export const WithText = () => <Button>Hello Button</Button>;
 
-export const withEmoji = () => (
+export const WithEmoji = () => (
   <Button>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
   </Button>
 );
+
+storiesOf('Buttons', module).add('With Text', () => <WithText />);
+storiesOf('Buttons', module).add('With emoji', () => <WithEmoji />);
