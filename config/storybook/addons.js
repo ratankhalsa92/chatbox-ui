859f0c55e8e8f04c3b4edf248126c8b79a32e571
Amit Rajput <amitrajput123.ar@gmail.com>
2020-04-30 00:51:07 +0530

    moving all js files to tsx

--- config/storybook/addons.js
+++ config/storybook/addons.js
@@ -1,7 +1,5 @@
 import '@storybook/addon-actions/register';
-import '@storybook/addon-links/register';
 import '@storybook/addon-backgrounds/register';
 import '@storybook/addon-viewport/register';
 import 'storybook-readme/register';
 import 'storybook-addon-jsx/register';
-import 'storybook-addon-figma/register';
